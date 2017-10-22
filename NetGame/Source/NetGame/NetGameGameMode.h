701253df69e5ff783a47f7caef01a0b9f9d04d21
Paul Tarr <fusionpizza00@gmail.com>
2017-10-22 02:30:45 -0700

    Setup

---
+++ NetGame/Source/NetGame/NetGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NetGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANetGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANetGameGameMode();
+};
+
+
+
